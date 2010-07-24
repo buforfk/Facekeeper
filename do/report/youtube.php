e9f122d77029d883f5fdefc0d727235f64966904
bu <bu@hax4.in>
2010-07-24 14:39:27 +0800

    增加分頁功能

--- do/report/youtube.php
+++ do/report/youtube.php
@@ -18,11 +18,24 @@ class report_youtube extends Event
     }
     public function defaultAction()
     {
-        $youtube_obj = new Youtube($this->db);
+        $start = $this->request->get('page', 1 , bP_INT);
+        $per = $this->config->get('pager.itemperpage');
+
+        $pager_object = new bPack_Pager();
+
+        $results_count = $this->db->query("SELECT count(*) as `count` FROM `youtube_pool`;")->fetch(PDO::FETCH_ASSOC);
+        $pager_object->total(ceil($results_count['count'] / $per));
+        $pager_object->per($per);
+        $pager_object->current($start);
         
-        $results = $youtube_obj->getAll();
-        
-        $this->view->assign('result',$results);
+        $start = ($start-1) * $per;
+        $this->view->assign('pager', $pager_object->output(new bP_Pager_Decorator_Pagi));
+        # 收錄資料統計
+        $results = $this->db->query("SELECT * FROM `youtube_pool` ORDER BY `date`,`views` DESC LIMIT $start,$per")->fetchAll(PDO::FETCH_ASSOC);
+
+        $this->view->assign('result' , $results);
+
+        $this->view->output('report/web.html');
         
         $this->view->output('report/youtube.html');
     }
