eb47301e4e6403fcb83a1eac4263fdaf7b27dd3b
bu <bu@hax4.in>
2010-07-30 18:32:37 +0800

    FK_FB 的 debug 修正

--- Engine/lib/FK_FB.py
+++ Engine/lib/FK_FB.py
@@ -90,5 +90,4 @@ class Parser:
 
     def writeToDB(self, directory):
         for item in directory:
-            print item[1]
-            self.db.execute( "INSERT INTO `fb_directories` SET `url` = '" + item[0].replace('\\/','/').replace('&amp;','') + "', `title` = '" + item[1].encode("utf-8") + "', `type` = '" + str(self.fetcher.type_id) + "', `tracking` = 0;")            
+            self.db.execute( "INSERT INTO `fb_directories` SET `url` = '" + item[0].replace('\\/','/').replace('&amp;','') + "', `title` = '" + item[1] + "', `type` = '" + str(self.fetcher.type_id) + "', `tracking` = 0;")            
