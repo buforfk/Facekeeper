c0d780a16f5369fdfa3d0ac8b1bcbf7d29766223
bu <bu@hax4.in>
2010-07-30 18:21:15 +0800

    修正 FK_FB 問題，加入除錯語法

--- Engine/lib/FK_FB.py
+++ Engine/lib/FK_FB.py
@@ -90,4 +90,5 @@ class Parser:
 
     def writeToDB(self, directory):
         for item in directory:
-            self.db.execute( "INSERT INTO `fb_directories` SET `url` = '" + item[0].replace('\\/','/').replace('&amp;','') + "', `title` = '" + str(item[1].encode('utf-8')) + "', `type` = '" + str(self.fetcher.type_id) + "', `tracking` = 0;")            
+            print item
+            self.db.execute( "INSERT INTO `fb_directories` SET `url` = '" + item[0].replace('\\/','/').replace('&amp;','') + "', `title` = '" + str(item[1]).encode('utf-8') + "', `type` = '" + str(self.fetcher.type_id) + "', `tracking` = 0;")            
