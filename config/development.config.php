eab634da04eb6779eda9c8ac45a0ff97a1b3babf
bu <bu@hax4.in>
2010-09-12 23:27:56 +0800

    修改對應的上線設定

--- config/development.config.php
+++ config/development.config.php
@@ -1,5 +1,5 @@
 <?php
-define('bPack_DEBUG_MODE', true);
+define('bPack_DEBUG_MODE', false);
 define('bPack_BASE_URI','/Facekeeper/');
 define('bPack_BaseDir',bPack_App_BaseDir . 'lib/bPack/');
 define('bPack_App_ConfigDir',bPack_App_BaseDir . 'config/');
