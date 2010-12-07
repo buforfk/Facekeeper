1f827b2a0e23584627af4ff36c12a83202781d20
facekeeper <facekeeper@facekeeper.(none)>
2010-12-07 12:29:04 +0800

    config files

---
+++ config/constant.php
@@ -0,0 +1,5 @@
+<?php
+define('ADMINISTRATOR_TABLE','administrators');
+define('KEYWORD_TABLES','keywords');
+define('CONFIG_TABLE','configs');
+
---
+++ config/production.config.php
@@ -0,0 +1,6 @@
+<?php
+define('bPack_DEBUG_MODE', true);
+define('bPack_BASE_URI','/Facekeeper/');
+define('bPack_BaseDir',bPack_App_BaseDir . 'lib/bPack/');
+define('bPack_App_ConfigDir',bPack_App_BaseDir . 'config/');
+define('bPack_TIMEZONE', 'Asia/Taipei');
